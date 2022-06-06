29019a222b97f87e23f196b82d8f0e8d020819df
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2022-06-06 23:59:27 +0530

    Increase expiry time of access tokens to 30 mins

--- domain_finder/settings.py
+++ domain_finder/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=360),
 }
 
